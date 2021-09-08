794f922553759de953b410e46adf793c953a670c
Danil Kolumbet <kolumbetko@gmail.com>
2021-09-08 05:43:14 +0300

    Add canvas to display individual bits

--- inspector.js
+++ inspector.js
@@ -2,6 +2,7 @@
 
 const listOfModulesElem = document.querySelector("#ListOfModulesElem")
 const displayElem = document.querySelector("#DisplayElem")
+const canvasElem = document.querySelector("#CanvasElem")
 
 function printByte(byte) {
     const span = document.createElement("span")
@@ -19,6 +20,7 @@ moduleNames && moduleNames.forEach(name => {
 })
 const moduleName = new URLSearchParams(location.search).get("module")
 if (moduleName) {
+    const widthOfWord = 16 * 2
     const scriptElem = document.createElement("script")
     scriptElem.src = `./${moduleName}/wasm-module-${moduleName}.js`
     document.body.appendChild(scriptElem)
@@ -29,8 +31,30 @@ if (moduleName) {
             if (i % 8 === 7) {
                 displayElem.appendChild(document.createTextNode(" "))
             }
-            if (i % (16 * 2) === 16 * 2 - 1) {
+            if (i % widthOfWord === widthOfWord - 1) {
                 displayElem.appendChild(document.createTextNode("\n"))
+            }
+        }
+        const ctx = canvasElem.getContext("2d", {alpha: false})
+        const scale = 3
+        const padding = 1
+        const width = widthOfWord * 8
+        const height = Math.ceil(view.length / width)
+        canvasElem.width = (padding + width + widthOfWord * 2) * scale
+        canvasElem.height = (padding + height) * 2 * scale * 2
+        ctx.fillStyle = "white"
+        ctx.fillRect(0, 0, canvasElem.width, canvasElem.height)
+        for (let i = 0; i < view.length; i++) {
+            const x = (i * 8) % width
+            const y = Math.floor((i * 8) / width)
+            // console.log(i, x, y)
+            for (let j = 0; j < 8; j++) {
+                if (view[i] & 2 ** j) {
+                    ctx.fillStyle = "black"
+                } else {
+                    ctx.fillStyle = "lightgrey"
+                }
+                ctx.fillRect((padding + x + j + Math.floor(x / widthOfWord) * scale) * scale + x / 2, (padding + y) * scale * 2, 2, 2)
             }
         }
     })
