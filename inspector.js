0429d0ac02ede3c35e666b0e10011a4d6f0d3571
Danil Kolumbet <kolumbetko@gmail.com>
2021-09-08 04:35:04 +0300

    Add module bytes display

--- inspector.js
+++ inspector.js
@@ -1,6 +1,14 @@
 "use strict"
 
 const listOfModulesElem = document.querySelector("#ListOfModulesElem")
+const displayElem = document.querySelector("#DisplayElem")
+
+function printByte(byte) {
+    const span = document.createElement("span")
+    span.textContent = byte.toString(16).padStart(2, "0") + " "
+    displayElem.appendChild(span)
+}
+
 moduleNames && moduleNames.forEach(name => {
     const link = document.createElement("a")
     link.href = `./inspector.html?module=${name}`
@@ -15,7 +23,15 @@ if (moduleName) {
     scriptElem.src = `./${moduleName}/wasm-module-${moduleName}.js`
     document.body.appendChild(scriptElem)
     scriptElem.addEventListener("load", e => {
-        console.log("script loaded")
-        console.log(moduleBuffer)
+        const view = new Uint8Array(moduleBuffer)
+        for (let i = 0; i < view.length; i++) {
+            printByte(view[i])
+            if (i % 8 === 7) {
+                displayElem.appendChild(document.createTextNode(" "))
+            }
+            if (i % (16 * 2) === 16 * 2 - 1) {
+                displayElem.appendChild(document.createTextNode("\n"))
+            }
+        }
     })
 }
