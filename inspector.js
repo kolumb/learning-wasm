a40f35ccbf024650f58d73df8bf435f718505a94
Danil Kolumbet <kolumbetko@gmail.com>
2021-09-11 05:45:23 +0300

    Add automatic header bytecode explanations

--- inspector.js
+++ inspector.js
@@ -3,11 +3,32 @@
 const listOfModulesElem = document.querySelector("#ListOfModulesElem")
 const displayElem = document.querySelector("#DisplayElem")
 const canvasElem = document.querySelector("#CanvasElem")
+const partsElem = document.querySelector("#PartsElem")
+
+function div(parent, text) {
+    const div = document.createElement("div")
+    div.textContent = text
+    parent.appendChild(div)
+}
+
+function byteStr(byte) {
+    return byte.toString(16).padStart(2, "0")
+}
 
 function printByte(byte) {
     const span = document.createElement("span")
-    span.textContent = byte.toString(16).padStart(2, "0") + " "
+    span.textContent = byteStr(byte) + " "
     displayElem.appendChild(span)
+}
+
+function getType(byte) {
+    switch (byte) {
+        case 0x7f: return "i32"
+        case 0x7e: return "i64"
+        case 0x7d: return "f32"
+        case 0x7c: return "f64"
+        default: return "unknown"
+    }
 }
 
 moduleNames && moduleNames.forEach(name => {
@@ -25,6 +46,55 @@ if (moduleName) {
     document.body.appendChild(scriptElem)
     scriptElem.addEventListener("load", e => {
         const view = new Uint8Array(moduleBuffer)
+        let index = 8
+        const signatureAndVersion = Array.from(view.slice(0, index)).map(byteStr).join(" ")
+        if (signatureAndVersion === "00 61 73 6d 01 00 00 00") {
+            div(partsElem, "00 61 73 6d ⠀ 01 00 00 00" + " — asm version 1")
+        } else {
+            partsElem.textContent = "Unknown file format"
+            return;
+        }
+        div(partsElem, `${byteStr(view[index])} — ${view[index] === 1 ? "start of section with types" : "that's something new.. Expected 01"}`)
+        index++
+        div(partsElem, `⠀ ${byteStr(view[index])} — number of bytes with types (${view[index]})`)
+        index++
+        const numberOfTypes = view[index]
+        div(partsElem, `⠀ ${byteStr(view[index])} — number of types (${view[index]})`)
+        index++
+        for (let i = 0; i < numberOfTypes; i++) {
+            div(partsElem, `⠀ ⠀ ${byteStr(view[index])} — ${view[index] === 0x60 ? "function type" : "what is this? I know only 60"}`)
+            index++
+            const arity = view[index]
+            div(partsElem, `⠀ ⠀ ⠀ ${Array.from(view.slice(index, index + arity + 1)).map(byteStr).join(" ")} — ${arity} arguments of type ${Array.from(view.slice(index + 1, index + arity + 1)).map(getType).join(", ")}`)
+            index += arity + 1
+            const outputs = view[index]
+            div(partsElem, `⠀ ⠀ ⠀ ${Array.from(view.slice(index, index + outputs + 1)).map(byteStr).join(" ")} — ${outputs} output of type ${Array.from(view.slice(index + 1, index + outputs + 1)).map(getType).join(", ")}`)
+            index += outputs + 1
+        }
+
+        div(partsElem, `${byteStr(view[index])} — ${view[index] === 3 ? "start of section with function declarations" : "that's something new. Expected 03"}`)
+        index++
+        div(partsElem, `⠀ ${byteStr(view[index])} — number of bytes with function declarations (${view[index]})`)
+        index++
+        const numberOfFunctions = view[index]
+        div(partsElem, `⠀ ${Array.from(view.slice(index, index + numberOfFunctions + 1)).map(byteStr).join(" ")} — ${numberOfFunctions} functions with types ${Array.from(view.slice(index + 1, index + numberOfFunctions + 1)).join(", ")}`)
+        index += numberOfFunctions + 1
+
+        div(partsElem, `${byteStr(view[index])} — ${view[index] === 7 ? "start of section with exports" : "that's something new. Expected 07"}`)
+        index++
+        div(partsElem, `⠀ ${byteStr(view[index])} — number of bytes with exports (${view[index]})`)
+        index++
+        const numberOfExports = view[index]
+        div(partsElem, `⠀ ${byteStr(view[index])} — number of exports (${view[index]})`)
+        index++
+        for (let i = 0; i < numberOfExports; i++) {
+            const lengthOfName = view[index]
+            div(partsElem, `⠀ ⠀ ${byteStr(view[index])} — length of name (${view[index]})`)
+            index++
+            div(partsElem, `⠀ ⠀ ⠀ ${Array.from(view.slice(index, index + lengthOfName + 1)).map(byteStr).join(" ")} — exported function "${Array.from(view.slice(index, index + lengthOfName)).map(c => String.fromCharCode(c)).join("")}" with index ${view[index + lengthOfName + 1]}`)
+            index += lengthOfName + 2
+        }
+
         const wordWidth = 8
         const numberOfWords = 2
         const widthOfLine = wordWidth * numberOfWords
