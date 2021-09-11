b8a926c0243df424b79300ac0c248a72c2d863f3
Danil Kolumbet <kolumbetko@gmail.com>
2021-09-11 06:00:45 +0300

    Refactor div function to remove braille space symbols

--- inspector.js
+++ inspector.js
@@ -5,9 +5,15 @@ const displayElem = document.querySelector("#DisplayElem")
 const canvasElem = document.querySelector("#CanvasElem")
 const partsElem = document.querySelector("#PartsElem")
 
-function div(parent, text) {
+function div(parent, code, text) {
+    const codeElem = document.createElement("code")
+    codeElem.textContent = code
+    const pre = document.createElement("pre")
+    pre.appendChild(codeElem)
+    const textNode = document.createTextNode(text)
     const div = document.createElement("div")
-    div.textContent = text
+    div.appendChild(pre)
+    div.appendChild(textNode)
     parent.appendChild(div)
 }
 
@@ -49,49 +55,49 @@ if (moduleName) {
         let index = 8
         const signatureAndVersion = Array.from(view.slice(0, index)).map(byteStr).join(" ")
         if (signatureAndVersion === "00 61 73 6d 01 00 00 00") {
-            div(partsElem, "00 61 73 6d ⠀ 01 00 00 00" + " — asm version 1")
+            div(partsElem, "00 61 73 6d   01 00 00 00", " — asm version 1")
         } else {
             partsElem.textContent = "Unknown file format"
             return;
         }
-        div(partsElem, `${byteStr(view[index])} — ${view[index] === 1 ? "start of section with types" : "that's something new.. Expected 01"}`)
+        div(partsElem, `${byteStr(view[index])}`, ` — ${view[index] === 1 ? "start of section with types" : "that's something new.. Expected 01"}`)
         index++
-        div(partsElem, `⠀ ${byteStr(view[index])} — number of bytes with types (${view[index]})`)
+        div(partsElem, `  ${byteStr(view[index])}`, ` — number of bytes with types (${view[index]})`)
         index++
         const numberOfTypes = view[index]
-        div(partsElem, `⠀ ${byteStr(view[index])} — number of types (${view[index]})`)
+        div(partsElem, `  ${byteStr(view[index])}`, ` — number of types (${view[index]})`)
         index++
         for (let i = 0; i < numberOfTypes; i++) {
-            div(partsElem, `⠀ ⠀ ${byteStr(view[index])} — ${view[index] === 0x60 ? "function type" : "what is this? I know only 60"}`)
+            div(partsElem, `    ${byteStr(view[index])}`, ` — ${view[index] === 0x60 ? "function type" : "what is this? I know only 60"}`)
             index++
             const arity = view[index]
-            div(partsElem, `⠀ ⠀ ⠀ ${Array.from(view.slice(index, index + arity + 1)).map(byteStr).join(" ")} — ${arity} arguments of type ${Array.from(view.slice(index + 1, index + arity + 1)).map(getType).join(", ")}`)
+            div(partsElem, `      ${Array.from(view.slice(index, index + arity + 1)).map(byteStr).join(" ")}`, ` — ${arity} arguments of type ${Array.from(view.slice(index + 1, index + arity + 1)).map(getType).join(", ")}`)
             index += arity + 1
             const outputs = view[index]
-            div(partsElem, `⠀ ⠀ ⠀ ${Array.from(view.slice(index, index + outputs + 1)).map(byteStr).join(" ")} — ${outputs} output of type ${Array.from(view.slice(index + 1, index + outputs + 1)).map(getType).join(", ")}`)
+            div(partsElem, `      ${Array.from(view.slice(index, index + outputs + 1)).map(byteStr).join(" ")}`, ` — ${outputs} output of type ${Array.from(view.slice(index + 1, index + outputs + 1)).map(getType).join(", ")}`)
             index += outputs + 1
         }
 
-        div(partsElem, `${byteStr(view[index])} — ${view[index] === 3 ? "start of section with function declarations" : "that's something new. Expected 03"}`)
+        div(partsElem, `${byteStr(view[index])}`, ` — ${view[index] === 3 ? "start of section with function declarations" : "that's something new. Expected 03"}`)
         index++
-        div(partsElem, `⠀ ${byteStr(view[index])} — number of bytes with function declarations (${view[index]})`)
+        div(partsElem, `  ${byteStr(view[index])}`, ` — number of bytes with function declarations (${view[index]})`)
         index++
         const numberOfFunctions = view[index]
-        div(partsElem, `⠀ ${Array.from(view.slice(index, index + numberOfFunctions + 1)).map(byteStr).join(" ")} — ${numberOfFunctions} functions with types ${Array.from(view.slice(index + 1, index + numberOfFunctions + 1)).join(", ")}`)
+        div(partsElem, `  ${Array.from(view.slice(index, index + numberOfFunctions + 1)).map(byteStr).join(" ")}`, ` — ${numberOfFunctions} functions with types ${Array.from(view.slice(index + 1, index + numberOfFunctions + 1)).join(", ")}`)
         index += numberOfFunctions + 1
 
-        div(partsElem, `${byteStr(view[index])} — ${view[index] === 7 ? "start of section with exports" : "that's something new. Expected 07"}`)
+        div(partsElem, `${byteStr(view[index])}`, ` — ${view[index] === 7 ? "start of section with exports" : "that's something new. Expected 07"}`)
         index++
-        div(partsElem, `⠀ ${byteStr(view[index])} — number of bytes with exports (${view[index]})`)
+        div(partsElem, `  ${byteStr(view[index])}`, ` — number of bytes with exports (${view[index]})`)
         index++
         const numberOfExports = view[index]
-        div(partsElem, `⠀ ${byteStr(view[index])} — number of exports (${view[index]})`)
+        div(partsElem, `  ${byteStr(view[index])}`, ` — number of exports (${view[index]})`)
         index++
         for (let i = 0; i < numberOfExports; i++) {
             const lengthOfName = view[index]
-            div(partsElem, `⠀ ⠀ ${byteStr(view[index])} — length of name (${view[index]})`)
+            div(partsElem, `    ${byteStr(view[index])}`, ` — length of name (${view[index]})`)
             index++
-            div(partsElem, `⠀ ⠀ ⠀ ${Array.from(view.slice(index, index + lengthOfName + 1)).map(byteStr).join(" ")} — exported function "${Array.from(view.slice(index, index + lengthOfName)).map(c => String.fromCharCode(c)).join("")}" with index ${view[index + lengthOfName + 1]}`)
+            div(partsElem, `      ${Array.from(view.slice(index, index + lengthOfName + 1)).map(byteStr).join(" ")}`, ` — exported function "${Array.from(view.slice(index, index + lengthOfName)).map(c => String.fromCharCode(c)).join("")}" with index ${view[index + lengthOfName + 1]}`)
             index += lengthOfName + 2
         }
 
