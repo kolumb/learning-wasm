6b08b0bb596ab69bfd16aed11844949e97bf6198
Danil Kolumbet <kolumbetko@gmail.com>
2021-09-15 04:08:10 +0300

    Apply indentation variable to all div calls

--- inspector/inspector.js
+++ inspector/inspector.js
@@ -61,83 +61,96 @@ if (moduleName) {
     document.body.appendChild(scriptElem)
     scriptElem.addEventListener("load", e => {
         const view = new Uint8Array(moduleBuffer)
+        let indent = 0
         let index = 8
         const signatureAndVersion = Array.from(view.slice(0, index)).map(byteStr).join(" ")
         if (signatureAndVersion === "00 61 73 6d 01 00 00 00") {
-            div(report, 0, "00 61 73 6d   01 00 00 00", "asm version 1")
+            div(report, indent, "00 61 73 6d   01 00 00 00", "asm version 1")
         } else {
             report.textContent = "Unknown file format"
             return;
         }
-        div(report, 0, `${byteStr(view[index])}`, `${view[index] === 1 ? "start of section with types" : "that's something new.. Expected 01"}`)
+        div(report, indent, `${byteStr(view[index])}`, `${view[index] === 1 ? "start of section with types" : "that's something new.. Expected 01"}`)
         index++
-        div(report, 1, `${byteStr(view[index])}`, `number of bytes with types (${view[index]})`)
+        indent++
+        div(report, indent, `${byteStr(view[index])}`, `number of bytes with types (${view[index]})`)
         index++
         const numberOfTypes = view[index]
-        div(report, 1, `${byteStr(view[index])}`, `number of types (${view[index]})`)
+        div(report, indent, `${byteStr(view[index])}`, `number of types (${view[index]})`)
         index++
+        indent++
         for (let i = 0; i < numberOfTypes; i++) {
-            div(report, 2, `${byteStr(view[index])}`, `${view[index] === 0x60 ? "function type" : "what is this? I know only 60"}`)
+            div(report, indent, `${byteStr(view[index])}`, `${view[index] === 0x60 ? "function type" : "what is this? I know only 60"}`)
             index++
+            indent++
             const arity = view[index]
-            div(report, 3, `${Array.from(view.slice(index, index + arity + 1)).map(byteStr).join(" ")}`, `${arity} arguments of type ${Array.from(view.slice(index + 1, index + arity + 1)).map(getType).join(", ")}`)
+            div(report, indent, `${Array.from(view.slice(index, index + arity + 1)).map(byteStr).join(" ")}`, `${arity} arguments of type ${Array.from(view.slice(index + 1, index + arity + 1)).map(getType).join(", ")}`)
             index += arity + 1
             const outputs = view[index]
-            div(report, 3, `${Array.from(view.slice(index, index + outputs + 1)).map(byteStr).join(" ")}`, `${outputs} output of type ${Array.from(view.slice(index + 1, index + outputs + 1)).map(getType).join(", ")}`)
+            div(report, indent, `${Array.from(view.slice(index, index + outputs + 1)).map(byteStr).join(" ")}`, `${outputs} output of type ${Array.from(view.slice(index + 1, index + outputs + 1)).map(getType).join(", ")}`)
             index += outputs + 1
+            indent--
         }
-
-        div(report, 0, `${byteStr(view[index])}`, `${view[index] === 3 ? "start of section with function declarations" : "that's something new. Expected 03"}`)
+        indent = 0
+        div(report, indent, `${byteStr(view[index])}`, `${view[index] === 3 ? "start of section with function declarations" : "that's something new. Expected 03"}`)
         index++
-        div(report, 1, `${byteStr(view[index])}`, `number of bytes with function declarations (${view[index]})`)
+        indent++
+        div(report, indent, `${byteStr(view[index])}`, `number of bytes with function declarations (${view[index]})`)
         index++
         const numberOfFunctions = view[index]
-        div(report, 1, `${Array.from(view.slice(index, index + numberOfFunctions + 1)).map(byteStr).join(" ")}`, `${numberOfFunctions} functions with types ${Array.from(view.slice(index + 1, index + numberOfFunctions + 1)).join(", ")}`)
+        div(report, indent, `${Array.from(view.slice(index, index + numberOfFunctions + 1)).map(byteStr).join(" ")}`, `${numberOfFunctions} functions with types ${Array.from(view.slice(index + 1, index + numberOfFunctions + 1)).join(", ")}`)
         index += numberOfFunctions + 1
 
-        div(report, 0, `${byteStr(view[index])}`, `${view[index] === 7 ? "start of section with exports" : "that's something new. Expected 07"}`)
+        indent = 0
+        div(report, indent, `${byteStr(view[index])}`, `${view[index] === 7 ? "start of section with exports" : "that's something new. Expected 07"}`)
         index++
-        div(report, 1, `${byteStr(view[index])}`, `number of bytes with exports (${view[index]})`)
+        indent++
+        div(report, indent, `${byteStr(view[index])}`, `number of bytes with exports (${view[index]})`)
         index++
         const numberOfExports = view[index]
-        div(report, 1, `${byteStr(view[index])}`, `number of exports (${view[index]})`)
+        div(report, indent, `${byteStr(view[index])}`, `number of exports (${view[index]})`)
         index++
+        indent++
         for (let i = 0; i < numberOfExports; i++) {
             const lengthOfName = view[index]
-            div(report, 2, `${byteStr(view[index])}`, `length of name (${view[index]})`)
+            div(report, indent, `${byteStr(view[index])}`, `length of name (${view[index]})`)
             index++
-            div(report, 3, `${Array.from(view.slice(index, index + lengthOfName + 2)).map(byteStr).join(" ")}`, `exported function "${Array.from(view.slice(index, index + lengthOfName)).map(c => String.fromCharCode(c)).join("")}" with index ${view[index + lengthOfName + 1]}`)
+            indent++
+            div(report, indent, `${Array.from(view.slice(index, index + lengthOfName + 2)).map(byteStr).join(" ")}`, `exported function "${Array.from(view.slice(index, index + lengthOfName)).map(c => String.fromCharCode(c)).join("")}" with index ${view[index + lengthOfName + 1]}`)
             index += lengthOfName + 2
+            indent--
         }
 
-        div(report, 0, `${byteStr(view[index])}`, `${view[index] === 10 ? "start of section with code" : "that's something new. Expected 0a"}`)
+        indent = 0
+        div(report, indent, `${byteStr(view[index])}`, `${view[index] === 10 ? "start of section with code" : "that's something new. Expected 0a"}`)
         index++
-        div(report, 1, `${byteStr(view[index])}`, `number of bytes with code (${view[index]})`)
+        indent++
+        div(report, indent, `${byteStr(view[index])}`, `number of bytes with code (${view[index]})`)
         index++
         
-        div(report, 0, `${Array.from(view.slice(index)).map(byteStr).join(" ")}`, "")
+        indent = 0
+        div(report, indent, `${Array.from(view.slice(index)).map(byteStr).join(" ")}`, "")
         const codeStrings = Array.from(view.slice(index)).map(byteStr)
         const states = {opcode: "opcode", datalen: "datalen"}
         let state = states.opcode
-        let indentation = 0
         codeStrings.forEach(byte => {
             switch (state) {
             case states.opcode:
                 switch (byte) {
                 case "01":
-                    div(report, indentation, `${byte}`, `module start`)
+                    div(report, indent, `${byte}`, `module start`)
                     state = states.datalen
                     break
                 case "03":
-                    div(report, indentation, `${byte}`, `function call`)
+                    div(report, indent, `${byte}`, `function call`)
                     break
                 default:
 
                 }
                 break
             case states.datalen:
-                div(report, indentation, `${byte}`, `length of data`)
-                indentation++
+                div(report, indent, `${byte}`, `length of data`)
+                indent++
                 state = states.opcode
                 break
             default:
